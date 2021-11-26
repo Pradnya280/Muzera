e951d7eb565503ac19950528733ddde16c46caba
Pradnya280 <78024989+Pradnya280@users.noreply.github.com>
2021-11-26 12:24:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 console.log("Welcome to Muzera");
 //initialize the variables
 let songIndex = 0;
-let audioElement = new Audio('song/1.mp3');
+let audioElement = new Audio('1.mp3');
 let masterPlay = document.getElementById('masterPlay');
 let myProgressBar = document.getElementById('myProgressBar');
 let gif1 = document.getElementById('gif1');
